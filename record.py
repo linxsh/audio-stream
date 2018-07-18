f231d0886aba61aa03c46423991b47bfd9664561
linxsh <linxsh@nationalchip.com>
2018-07-19 06:47:37 +0800

    fix audio record

--- record.py
+++ record.py
@@ -2,31 +2,34 @@ from pyaudio import PyAudio,paInt16
 import numpy as np
 import wave
 
-framerate=8000
-framelength=400
-frameshift=400
+framerate=16000
 channel=1
 samplewidth=2
-samplepoints=200
+samplepoints=400
+sampleshifts=160
 
 class AudioRecord(object):
-    def __init__(self, filename=None):
+    def __init__(self):
         pa=PyAudio()
-        self.stream=pa.open(format = paInt16, channels = channel, rate=framerate, input=True, frames_per_buffer=framelength)
-        self.pabuf=[]
-        if filename is not None:
-            self.wavefile=wave.open(filename,'wb')
-            self.wavefile.setnchannels(channel)
-            self.wavefile.setsampwidth(samplewidth)
-            self.wavefile.setframerate(framerate)
+        self.stream=pa.open(format = paInt16, channels = channel, rate=framerate, input=True, frames_per_buffer=samplepoints)
+        self.save_audio_array=np.zeros(0, dtype=np.short)
 
     def read_frame(self):
-        if len(self.pabuf) < framelength:
-          audio_data=self.stream.read(samplepoints)
-          self.pabuf.extend(audio_data)
-        rbuf=self.pabuf[:framelength]
-        del self.pabuf[:frameshift]
-        return np.array(rbuf)
+        if np.size(self.save_audio_array) < samplepoints:
+            audio_string=self.stream.read(samplepoints)
+            audio_array=np.fromstring(audio_string, dtype=np.short)
+            self.save_audio_array=np.append(self.save_audio_array, audio_array)
+            #print("read %d"%(np.size(audio_array)))
+        read_audio_array = self.save_audio_array[:samplepoints]
+        self.save_audio_array=self.save_audio_array[sampleshifts:]
+        #print("write %d, res: %d"%(np.size(read_audio_array), np.size(self.save_audio_array)))
+        return read_audio_array
 
-    def save_to_file(self, data):
-        self.wavefile.writeframes(b"".join(data.tolist()))
+    def save_to_file(self, filename=None, data=np.zeros(0, dtype=np.short)):
+        if filename is not None:
+            wavefile=wave.open(filename,'wb')
+            wavefile.setnchannels(channel)
+            wavefile.setsampwidth(samplewidth)
+            wavefile.setframerate(framerate)
+            wavefile.writeframes(b"".join(data.tostring()))
+            wavefile.close()
--- sample.py
+++ sample.py
@@ -2,6 +2,6 @@ from record import AudioRecord
 import numpy as np
 
 if __name__ == '__main__':
-    arec=AudioRecord(filename="./a.wav")
+    arec=AudioRecord()
     while True:
-        arec.save_to_file(data=arec.read_frame())
+        data=arec.read_frame()
